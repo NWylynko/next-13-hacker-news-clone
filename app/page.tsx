27a1b3cddcd6cf46b71a5c7544ace9e9ac39f494
Nick Wylynko <nick@wylynko.com>
2022-10-26 18:02:04 +0800

    Add links to out of site until we build clones for those links

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { getStory } from "../lib/getStory";
 import { getTopStories } from "../lib/getTopStories";
 
@@ -25,13 +26,21 @@ async function Item({ id, index }: { id: number, index: number }) {
     <div style={{ margin: 8 }}>
       <div style={{ display: "flex", gap: 8 }}>
         <span>{index + 1}.</span>
-        <h4 style={{ margin: 0 }}>{story.title}</h4>
-        <span>({story.domain})</span>
+        <Link href={story.url} style={{ color: "black", textDecoration: "none" }}>
+          <h4 style={{ margin: 0 }}>{story.title}</h4>
+        </Link>
+        <span>
+          (
+            <Link style={{ color: "black" }} href={`https://news.ycombinator.com/from?site=${story.domain}`}>
+              {story.domain}
+            </Link>
+          )
+        </span>
       </div>
       <div>
-        <span>{story.score} points by {story.by} at {story.time} | {story.numOfComments} comments</span>
+        <span>{story.score} points by <Link href={`https://news.ycombinator.com/user?id=${story.by}`} style={{ color: "black" }}>{story.by}</Link> at {story.time} | <Link href={`https://news.ycombinator.com/item?id=${id}`} style={{ color: "black" }}>{story.numOfComments} comments</Link></span>
       </div>
     </div>
-    )
+  )
 
 }
